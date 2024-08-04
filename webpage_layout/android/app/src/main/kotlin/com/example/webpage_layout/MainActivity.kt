562559bceb6da30b873c7232a134f02b9348cb48
mskwon02 <121784261+mskwon02@users.noreply.github.com>
2024-08-04 19:00:50 +0900

    web version layout first

---
+++ webpage_layout/android/app/src/main/kotlin/com/example/webpage_layout/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.webpage_layout
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ webpage_layout/lib/main.dart
@@ -0,0 +1,119 @@
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MainPage(),
+    );
+  }
+}
+
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        toolbarHeight: 60,//앱바 세로 크기
+        leadingWidth: 250,//리딩영역 가로
+        leading: Container(
+          margin: EdgeInsets.all(10),
+          child: Lottie.asset('lottie/logo.json'),
+        ),
+        title: Container(
+          height: 40,
+          width: 500,
+          child: TextField(
+            decoration: InputDecoration(
+              filled: true,
+              fillColor: Colors.grey[100],
+              hintText: 'Search',
+              prefixIcon: Icon(Icons.search),
+              enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide.none,
+                  borderRadius: BorderRadius.circular(70)),
+            ),
+          ),
+        ),
+        actions: [
+          Container(
+              margin: EdgeInsets.only(right: 50, left: 50),
+              child: Icon(Icons.message)),
+          Container(
+              margin: EdgeInsets.only(right: 50, left: 50),
+              child: Icon(Icons.person))
+        ],
+      ),
+      body: Row(
+        children: [
+          SideBar(),
+          Expanded(
+            child: Container(
+              color: Colors.grey[200],
+              child: Column(
+                children: [],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+class SideBar extends StatefulWidget {
+  const SideBar({super.key});
+
+  @override
+  State<SideBar> createState() => _SideBarState();
+}
+
+class _SideBarState extends State<SideBar> {
+  int selectedMenuIdx=0;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 250,
+      padding: EdgeInsets.all(10),
+      child: ListView.builder(
+          itemCount: 10,
+          itemBuilder: (context, index) {
+            return Container(
+              margin: EdgeInsets.all(10),
+              child: ListTile(
+                  tileColor: Colors.white,
+                  onTap: () {
+                    setState(() {
+                      selectedMenuIdx = index;
+                    });
+                  },
+                  leading: Icon(Icons.timer),
+                  title: Text("Dashboard"),
+                  selected: selectedMenuIdx == index,
+                  selectedTileColor: Colors.blue,
+                  //선택됐을 때 타일 색깔
+                  selectedColor: Colors.white,
+                  //선택됐을때 내용 색깔
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8),
+                  )),
+            );
+          }),
+    );
+  }
+}
