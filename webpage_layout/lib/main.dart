1b3a245622c19fee0f61205fc1fbd3f9de50856f
mskwon02 <121784261+mskwon02@users.noreply.github.com>
2024-08-05 16:23:18 +0900

    스크롤 기능 추가

--- webpage_layout/lib/main.dart
+++ webpage_layout/lib/main.dart
@@ -27,40 +27,40 @@ class _MainPageState extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
         backgroundColor: Colors.white,
-        toolbarHeight: 60,//앱바 세로 크기
-        leadingWidth: 250,//리딩영역 가로
-        leading: Container(
-          margin: EdgeInsets.all(10),
-          child: Lottie.asset('lottie/logo.json'),
-        ),
-        title: Container(
-          height: 40,
-          width: 500,
-          child: TextField(
-            decoration: InputDecoration(
-              filled: true,
-              fillColor: Colors.grey[100],
-              hintText: 'Search',
-              prefixIcon: Icon(Icons.search),
-              enabledBorder: OutlineInputBorder(
-                  borderSide: BorderSide.none,
-                  borderRadius: BorderRadius.circular(70)),
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          toolbarHeight: 60,//앱바 세로 크기
+          leadingWidth: 250,//리딩영역 가로
+          leading: Container(
+            margin: EdgeInsets.all(10),
+            child: Lottie.asset('lottie/logo.json'),
+          ),
+          title: Container(
+            height: 40,
+            width: 500,
+            child: TextField(
+              decoration: InputDecoration(
+                filled: true,
+                fillColor: Colors.grey[100],
+                hintText: 'Search',
+                prefixIcon: Icon(Icons.search),
+                enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide.none,
+                    borderRadius: BorderRadius.circular(70)),
+              ),
             ),
           ),
+          actions: [
+            Container(
+                margin: EdgeInsets.only(right: 50, left: 50),
+                child: Icon(Icons.message)),
+            Container(
+                margin: EdgeInsets.only(right: 50, left: 50),
+                child: Icon(Icons.person))
+          ],
         ),
-        actions: [
-          Container(
-              margin: EdgeInsets.only(right: 50, left: 50),
-              child: Icon(Icons.message)),
-          Container(
-              margin: EdgeInsets.only(right: 50, left: 50),
-              child: Icon(Icons.person))
-        ],
-      ),
-      body: SideBar()
+        body: SideBar()
     );
   }
 }
@@ -110,20 +110,22 @@ class _SideBarState extends State<SideBar> {
           child: Container(
             padding: EdgeInsets.all(10),
             color: Colors.grey[200],
-            child: Column(
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    CardSmall(),
-                    CardSmall(),
-                    CardSmall(),
-                    CardSmall()
-                  ],
-                ),
-                CardLarge(),
-                CardLarge()
-              ],
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Expanded(child: CardSmall()),
+                      Expanded(child: CardSmall()),
+                      Expanded(child: CardSmall()),
+                      Expanded(child: CardSmall())
+                    ],
+                  ),
+                  CardLarge(),
+                  CardLarge()
+                ],
+              ),
             ),
           ),
         )
@@ -137,14 +139,12 @@ class CardSmall extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-      child: Card(
-        margin: EdgeInsets.all(10),
-        child: Container(
-          height: 150,
-        ),
-        color: Colors.white,
+    return Card(
+      margin: EdgeInsets.all(10),
+      child: Container(
+        height: 150,
       ),
+      color: Colors.white,
     );
   }
 }
@@ -154,14 +154,12 @@ class CardLarge extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-      child: Card(
-        margin: EdgeInsets.all(10),
-        child: Container(
-          height: 200,
-        ),
-        color: Colors.white,
+    return Card(
+      margin: EdgeInsets.all(10),
+      child: Container(
+        height: 300,
       ),
+      color: Colors.white,
     );
   }
 }
